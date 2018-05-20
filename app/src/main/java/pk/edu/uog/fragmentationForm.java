a29c9a0bfe585a0299dd0c4d43d89c026c57b587
Rida Shahid <14051598-020@uog.edu.pk>
2018-05-20 22:14:56 +0500

    Navigation Drawer added. Map added. some issue fixed.

--- app/src/main/java/pk/edu/uog/fragmentationForm.java
+++ app/src/main/java/pk/edu/uog/fragmentationForm.java
@@ -1,8 +1,25 @@
 package pk.edu.uog;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by RIDA on 5/16/2018.
  */
 
-class fragmentationForm {
+public class fragmentationForm extends Fragment {
+
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView= inflater.inflate(R.layout.activity_fragmentation_form, container,false);
+        return myView;
+    }
 }
